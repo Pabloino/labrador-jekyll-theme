330ae020af7c7861462dcd558304036395043f4b
Pablo <pablo-ino@hotmail.com>
2018-02-12 18:27:00 -0300

    Update labrador.js

--- assets/js/labrador.js
+++ assets/js/labrador.js
@@ -13,11 +13,11 @@ $(document).ready(function () {
 	$(window).scroll(function (event) {
 		var scroll = $(window).scrollTop();
 		// ABOUT
-		if (scroll >= 400) $('.text-about p').addClass('show');
-		if (scroll <= 200) $('.text-about p').removeClass('show');
+		if (scroll >= 400) { $('.text-about p').addClass('show'); $('.section-bullets').removeClass('section-bullets-white'); }
+		if (scroll <= 200) { $('.text-about p').removeClass('show'); $('.section-bullets').addClass('section-bullets-white'); }
 		// BASE
-		if (scroll >= 1200) $('.text-base p').addClass('show');
-		if (scroll <= 1000) $('.text-base p').removeClass('show');
+		if (scroll >= 1000) $('.text-base p').addClass('show');
+		if (scroll <= 800) $('.text-base p').removeClass('show');
 		// QUOTE
 		if (scroll >= 2000) { $('.text-quote p').addClass('show'); $('.section-bullets').addClass('section-bullets-white'); }
 		if (scroll >= 2800 || scroll <= 1200) { $('.text-quote p').removeClass('show');$('.section-bullets').removeClass('section-bullets-white'); }
