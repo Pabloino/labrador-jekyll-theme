1f8b88c74103504d3c849566f895fc90d1c6e122
Pablo <pablo-ino@hotmail.com>
2018-02-19 02:45:16 -0300

    Update labrador.js

--- assets/js/labrador.js
+++ assets/js/labrador.js
@@ -10,6 +10,9 @@ $(document).ready(function () {
 		// PROCESAMIENTO
 		if (scroll > 1600) { $('.text-preprouno p').addClass('show'); $('.section-bullets').addClass('section-bullets-black'); }
 		if ((scroll >= 800 && scroll <= 1400) || scroll >= 7400) { $('.text-preprouno p').removeClass('show'); $('.section-bullets').removeClass('section-bullets-black'); }
+		// AGRUPACION
+		if (scroll > 7100) { $('.text-agrup p').addClass('show'); }
+		if ((scroll >= 6300 && scroll <= 6900) || scroll >= 8000) { $('.text-agrup p').removeClass('show'); }
 		// LANGUAGE
 		if (scroll >= 15580) { $('.profile-photo').addClass('profile-photo-white'); }
 		if (scroll >= 16280 || scroll <= 15580) { $('.profile-photo').removeClass('profile-photo-white'); }
